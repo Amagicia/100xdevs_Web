32769a1e7a301fddf64397e93a5e1e4580159cb4
Aman Tiwari <amntwri0323@gmail.com>
2026-02-04 16:33:30 +0530

    solution 5 added

--- Week-02/JS-Basic/20_intermediate_(Objects + Arrays)/05_Transform_array_of_orders_into_revenue_per_category.js
+++ Week-02/JS-Basic/20_intermediate_(Objects + Arrays)/05_Transform_array_of_orders_into_revenue_per_category.js
@@ -17,3 +17,20 @@
         { electronics: 300, clothes: 50 }
         ```
  */
+
+const data = [
+    { id: 1, category: "electronics", price: 100 },
+    { id: 2, category: "clothes", price: 50 },
+    { id: 3, category: "electronics", price: 200 },
+];
+let ans = {};
+
+for (const key of data) {
+    // console.log(key);
+    if (!ans[key.category]) {
+        ans[key.category] = key.price;
+    } else {
+        ans[key.category] = ans[key.category] + key.price;
+    }
+}
+console.log(ans);
