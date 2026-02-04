f4f17cc145e42e51a5d777b6e01b2d8fe62a0568
Aman Tiwari <amntwri0323@gmail.com>
2026-02-04 16:07:57 +0530

    solution 3 added

--- Week-02/JS-Basic/20_intermediate_(Objects + Arrays)/03_Remove_falsy_values_from_object.js
+++ Week-02/JS-Basic/20_intermediate_(Objects + Arrays)/03_Remove_falsy_values_from_object.js
@@ -13,3 +13,14 @@
         { c: "hello", e: 5 }
         ```
          */
+
+const data = { a: 0, b: null, c: "hello", d: undefined, e: 5 };
+let ans = {};
+
+for (const [key, values] of Object.entries(data)) {
+    console.log(key, values);
+    if (values !== null && values !== undefined && values != 0) {
+        ans[key] = values;
+    }
+}
+console.log(ans);
