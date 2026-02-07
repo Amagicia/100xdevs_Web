61f4f8795df5945421e2ffdc6cb9b18ce287c8f0
Aman Tiwari <amntwri0323@gmail.com>
2026-02-07 17:47:25 +0530

    Week-2 [Assignment 3][5] palindrome

--- Week-02/01-js/medium/palindrome.js
+++ Week-02/01-js/medium/palindrome.js
@@ -6,8 +6,30 @@
   - `npm run test-palindrome`
 */
 
-function isPalindrome(str) {
+function isPalindrome(s) {
+    let str = s.toLowerCase().replace(/[^a-z0-9]/gi, "") // remove spaces + punctuation
+    // * [^a-z0-9] matches all non‑alphanumeric characters,
+    // * and the global flag removes them from the entire string.”
+
+    let left = 0;
+    let right = str.length - 1;
+
+    while (left < right) {
+        if (str[left] !== str[right]) return false;
+        left++;
+        right--;
+    }
     return true;
 }
+
+// ? Better Approach By Str => Array => Reverse the array , Then compare
+/**
+ * function isPalindrome(s) {
+  let str = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return str === str.split("").reverse().join("");
+}
+ */
+let ans = isPalindrome("s             ss");
+console.log(ans);
 
 module.exports = isPalindrome;
