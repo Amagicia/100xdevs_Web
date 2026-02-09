88a3b1bf45fdc3d5c0d4817fd009e323d8fc0f9b
Aman Tiwari <amntwri0323@gmail.com>
2026-02-09 19:12:48 +0530

    week - 3 Callbacks Once

--- Week-03-JS-Architecture/callbacks/easy/once.js
+++ Week-03-JS-Architecture/callbacks/easy/once.js
@@ -2,12 +2,42 @@
 //
 // You are required to implement a wrapper function named once that accepts a
 // callback-based asynchronous function `fn`.
+
 // The wrapper should ensure that `fn` is executed only on the first call.
 // Any subsequent calls should not re-execute `fn` and should instead invoke
 // the callback with the same result (or error) from the first invocation.
 
-function once(fn) {
 
+function once(fn) {
+    let result, err;
+    let finish = false;
+    let stop = false;
+    let queue = [];
+
+    return function (...params) {
+        let callback = params.pop();
+
+        if (finish) {
+            return callback(err, result);
+        }
+        queue.push(callback);
+        if (stop) {
+            return;
+        }
+
+        stop = true;
+        // if (!finish) {
+        fn(...params, (e, res) => {
+            result = res;
+            err = e;
+            finish = true;
+            while (queue.length) {
+                let cb = queue.shift();
+                cb(err, result);
+            }
+        });
+    };
+    // };
 }
 
 module.exports = once;
