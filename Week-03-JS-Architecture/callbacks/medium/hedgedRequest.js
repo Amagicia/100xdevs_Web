8542198ea197f7fac2d15cc3dc52d4325b85992e
Aman Tiwari <amntwri0323@gmail.com>
2026-02-18 20:17:30 +0530

    week-3 Callbacks HedgedRequest solve

--- Week-03-JS-Architecture/callbacks/medium/hedgedRequest.js
+++ Week-03-JS-Architecture/callbacks/medium/hedgedRequest.js
@@ -11,6 +11,63 @@
 // - Start Secondary after timeoutMs if needed.
 // - First success wins.
 // - Callback must be called exactly once.
-function hedgedRequest(primary, secondary, timeoutMs, onComplete) {}
+// function hedgedRequest(primary, secondary, timeoutMs, onComplete) {
+//     let secondaryCalled = false;
+
+//     let timer = setTimeout(() => {
+//         if (!secondaryCalled) {
+//             secondary((err, res) => {
+//                 secondaryCalled=true;
+//                 if (!err) {
+//                     return onComplete(null, res);
+//                 }
+//                 return onComplete(err);
+//             });
+//         }
+//     }, timeoutMs);
+
+//     primary((err, res) => {
+//         if(!secondaryCalled){
+//             if (!err) {
+//                 secondaryCalled=true;
+//                 clearTimeout(timer);
+//                 return onComplete(null, res);
+//             }
+//         }
+//     });
+// }
+
+// ? Optimized way
+function hedgedRequest(primary, secondary, timeoutMs, onComplete) {
+    let finished = false;
+    let failures = 0;
+    let timer = null;
+
+    function done(err, res) {
+        if (finished) return;
+        finished = true;
+        clearTimeout(timer);
+        onComplete(err, res);
+    }
+
+    function handleResult(err, res) {
+        if (!err) {
+            done(null, res); // first success wins
+        } else {
+            failures++;
+            if (failures === 2) {
+                done(err); // only fail if both fail
+            }
+        }
+    }
+
+    // start primary immediately
+    primary(handleResult);
+
+    // start secondary after timeout
+    timer = setTimeout(() => {
+        secondary(handleResult);
+    }, timeoutMs);
+}
 
 module.exports = hedgedRequest;
